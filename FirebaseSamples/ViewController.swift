67e6a084b4ecad8f7b1bcd0f708c7b94b4249f42
Virtual Stacks <virtualstacks@vs.local>
2018-10-25 18:40:00 +0530

    Initial Commit

---
+++ FirebaseSamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseSamples
+//
+//  Created by VirtualStacks on 25/10/18.
+//  Copyright © 2018 VirtualStacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
